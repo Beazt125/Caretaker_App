4af43b24439d170b1a881cd50e69303acbd2bafa
Beazt125 <nathanyured@gmail.com>
2024-02-08 20:27:48 -0500

    w

--- app/src/main/java/com/example/caretakerapp/SymptomsList.java
+++ app/src/main/java/com/example/caretakerapp/SymptomsList.java
@@ -1,31 +1,61 @@
 package com.example.caretakerapp;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SymptomsList extends AppCompatActivity{
     RecyclerView recyclerView;
     TextView welcomeTextView;
     EditText messageEditText;
     ImageButton sendButton;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_symptoms_list);
+        messageList = new ArrayList<>();
 
         recyclerView = findViewById(R.id.recycler_view);
         welcomeTextView = findViewById(R.id.welcome_text);
         messageEditText = findViewById(R.id.message_edit_text);
         sendButton = findviewById(R.id.send_btn);
 
-        return sendButton.setOnClickListener((v) -> {
+        //setup recycler view
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm= new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+
+        sendButton.setOnClickListener((v) -> {
             String question = messageEditText.getText().toString().trim();
-            Toast.makeText(this,question,Toast.LENGTH_LONG).show();
+            addToChat(question, Message.SENT_BY_ME);
+            messageEditText.setText(" ");
+            welcomeTextView.setVisibility(View.GONE);
         });
     }
-}
+
+    void addToChat(String message, String sentby){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message, sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+
+    }
